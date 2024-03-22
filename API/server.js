d830aebdfe1ae78723e60fe6e60c0ed6c461cd65
lissacss <aliciacriss2807@gmail.com>
2024-03-21 22:02:06 -0300

    Integração de user e comments

--- API/server.js
+++ API/server.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql2')
-const cors = require('cors')
-require('dotenv').config()
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const cors = require("cors");
+require("dotenv").config();
 
 const server = express();
 
@@ -15,53 +14,76 @@ server.use(bodyParser.json());
 const PORT = 7000;
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 db.connect((err) => {
-    if (err) return console.log(err);
-    console.log('Conectado com sucesso!');
-})
-
-server.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    db.query('SELECT * FROM user WHERE username = ? AND password = ?',
-     [username, password], (err, results) => {
-        if (err) {
-            res.status(500).json({ success: false, error: 'Internal server error' });
-            return;
-        }
-        if (results.length > 0) {
-            const { id, username, firstname, lastname } = results[0];
-            res.json({ success: true, user: { id, username, firstname, lastname } });
-        } else {
-            res.json({ success: false, error: 'Usuário ou senha inválidos' });
-        }
-    })
-})
-
-server.get('/comment', (req, res) => {
-    db.query('SELECT * FROM comment', (err, results) => {
-        if (err) {
-            res.status(500).json({ success: false, error: 'Internal server error' });
-            return;
-        }
-
-        res.json({ success: true, comment: results });
-    });
+  if (err) return console.log(err);
+  console.log("Conectado com sucesso!");
 });
-server.get('/user', (req, res) => {
-    db.query('SELECT * FROM user', (err, results) => {
 
+server.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  db.query(
+    "SELECT * FROM user WHERE username = ? AND password = ?",
+    [username, password],
+    (err, results) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ success: false, error: "Internal server error" });
+        return;
+      }
+      if (results.length > 0) {
+        const { id, username, firstname, lastname } = results[0];
+        res.json({
+          success: true,
+          user: { id, username, firstname, lastname },
+        });
+      } else {
+        res.json({ success: false, error: "Usuário ou senha inválidos" });
+      }
+    }
+  );
+});
 
-        res.json({ success: true, user: results });
-    });
+server.post("/new-comment", (req, res) => {
+  const { author, comment_text } = req.body;
+  db.query(
+    "INSERT INTO comment (author,comment_text) VALUES (?,?)",
+    [author, comment_text],
+    (err, results) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ success: false, error: "Internal server error" });
+        return;
+      }
+      res.json({ success: true });
+    }
+  );
+});
+
+server.get("/comment", (req, res) => {
+  db.query("SELECT * FROM comment", (err, results) => {
+    if (err) {
+      res.status(500).json({ success: false, error: "Internal server error" });
+      return;
+    }
+
+    res.json({ success: true, comment: results });
+  });
+});
+server.get("/user", (req, res) => {
+  db.query("SELECT * FROM user", (err, results) => {
+    res.json({ success: true, user: results });
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`O server está rodando em http://localhost:${PORT}`)
-})
+  console.log(`O server está rodando em http://localhost:${PORT}`);
+});
--- FRONTEND/Login/LoginComponent.js
+++ FRONTEND/Login/LoginComponent.js
@@ -3,57 +3,57 @@ import { setInputComment } from "../Comment/CommentComponent.js";
 import { LoginService } from "../services/login.services.js";
 
 const getLoginInputs = () => {
-    return {
-        username: document.getElementById('username'),
-        password: document.getElementById('password')
-    }
-}
-
+  return {
+    username: document.getElementById("username"),
+    password: document.getElementById("password"),
+  };
+};
 
 const handleShowHide = () => {
-    const newCommentTag = document.getElementById('form-comentario');
-    const loginTag = document.getElementById('login-form');
-    if (newCommentTag.classList.contains('disabled')) {
-        newCommentTag.classList.remove('disabled');
-        loginTag.classList.add('disabled');
-    } else {
-        newCommentTag.classList.add('disabled');
-        loginTag.classList.remove('disabled');
-    }
-}
+  const newCommentTag = document.getElementById("form-comentario");
+  const loginTag = document.getElementById("login-form");
+  if (newCommentTag.classList.contains("disabled")) {
+    newCommentTag.classList.remove("disabled");
+    loginTag.classList.add("disabled");
+  } else {
+    newCommentTag.classList.add("disabled");
+    loginTag.classList.remove("disabled");
+  }
+};
 
 const handleLogin = (event) => {
-    event.preventDefault();
-    const { username, password } = getLoginInputs();
-    const user = new User(null, username.value, password.value)
+  event.preventDefault();
+  const { username, password } = getLoginInputs();
+  const user = new User(null, username.value, password.value);
 
-    LoginService.apiAuthUser(user).then(result => {
-        console.log(result)
-        user.setId(result.id);
-        user.setPassword(null);
-        user.setFirstname(result.firstname);
-        user.setLastname(result.lastname);
-        handleShowHide();
+  LoginService.apiAuthUser(user)
+    .then((result) => {
+      console.log(result);
+      user.setId(result.id);
+      user.setPassword(null);
+      user.setFirstname(result.firstname);
+      user.setLastname(result.lastname);
+      handleShowHide();
 
-        const setInputComment = document.getElementById('inputAuthor');
-        inputAuthor.value = result.firstname + ' ' + result.lastname;
-        inputAuthor.disabled = true;
-        inputAuthor.style.backgroundColor = '#faebd7';
-        inputAuthor.style.color = '#444';
-        console.log(user);
-    }).catch(error => {
-        alert(`Login inválido. Erro:${error.message}`)
+      const setInputComment = document.getElementById("inputAuthor");
+      inputAuthor.value = result.firstname + " " + result.lastname;
+      inputAuthor.disabled = true;
+      inputAuthor.style.backgroundColor = "#faebd7";
+      inputAuthor.style.color = "#444";
+      console.log(user);
     })
+    .catch((error) => {
+      alert(`Login inválido. Erro:${error.message}`);
+    });
 
-    console.log(user)
-}
-
+  console.log(user);
+};
 
 const LoginComponent = {
-    run: () => {
-        const formLogin = document.getElementById('formLogin');
-        formLogin.addEventListener('submit', handleLogin);
-    }
-}
+  run: () => {
+    const formLogin = document.getElementById("formLogin");
+    formLogin.addEventListener("submit", handleLogin);
+  },
+};
 
-export { LoginComponent }
+export { LoginComponent };

9b2e6a0ec8a56ad074b5101a89432d44f99c7d4c
lissacss <aliciacriss2807@gmail.com>
2024-03-27 22:14:03 -0300

    Tela UserData implementada para demonstração de dados do usuário.

--- FRONTEND/utils.js
+++ FRONTEND/utils.js
@@ -1,14 +1,32 @@
-const formatDate = (data) =>{
+const formatDate = (data) => {
     const currentDate = new Date(data);
-    const options = { 
+    const options = {
         day: '2-digit',
         month: 'short',
         hour: 'numeric',
         hour12: false
     };
 
-    let formattedDate = currentDate.toLocaleDateString('pt-BR',options)
+    let formattedDate = currentDate.toLocaleDateString('pt-BR', options)
     return formattedDate.replace(',', ' às') + 'hs';
 }
 
-export {formatDate}
+const randomInt = (value) => {
+    return Math.floor(Math.random() * value)
+}
+
+const randomColors = () => {
+    let darkPalette = ['191970', '000080', '00008B', '0000CD', '483D8B', '4682B4', '708090']
+    let lightPalette = ['B0C4DE', 'ADD8E6', '87CEEB', '87CEFA', '00BFFF', '6495ED', '778899']
+
+    let colors = []
+    colors.push(darkPalette)
+    colors.push(lightPalette)
+
+    return {
+        dark: colors[0][randomInt(colors[0].length)],
+        light: colors[1][randomInt(colors[1].length)]
+    }
+}
+
+export { formatDate, randomColors }
